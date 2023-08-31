a5e870d2d8d8b454e32f631732cc4f1e2cab6dff
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-08-31 22:09:03 +0530

    Time: 4 ms (31.48%) | Memory: 11.2 MB (19.41%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,27 +1,58 @@
 class Solution {
 public:
-    int search(vector<int>& A, int target) {
-        int n=A.size();
-        int lo=0,hi=n-1;
-        // find the index of the smallest value using binary search.
-        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
-        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
-        while(lo<hi){
-            int mid=(lo+hi)/2;
-            if(A[mid]>A[hi]) lo=mid+1;
-            else hi=mid;
+    int search(vector<int>& arr, int k) {
+         int n=arr.size();
+        // int lo=0,hi=n-1;
+        // // find the index of the smallest value using binary search.
+        // // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
+        // // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
+        // while(lo<hi){
+        //     int mid=(lo+hi)/2;
+        //     if(A[mid]>A[hi]) lo=mid+1;
+        //     else hi=mid;
+        // }
+        // // lo==hi is the index of the smallest value and also the number of places rotated.
+        // int rot=lo;
+        // lo=0;hi=n-1;
+        // // The usual binary search and accounting for rotation.
+        // while(lo<=hi){
+        //     int mid=(lo+hi)/2;
+        //     int realmid=(mid+rot)%n;
+        //     if(A[realmid]==target)return realmid;
+        //     if(A[realmid]<target)lo=mid+1;
+        //     else hi=mid-1;
+        // }
+        // return -1;
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return mid;
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
         }
-        // lo==hi is the index of the smallest value and also the number of places rotated.
-        int rot=lo;
-        lo=0;hi=n-1;
-        // The usual binary search and accounting for rotation.
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int realmid=(mid+rot)%n;
-            if(A[realmid]==target)return realmid;
-            if(A[realmid]<target)lo=mid+1;
-            else hi=mid-1;
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
         }
-        return -1;
+    }
+    return -1;
+
     }
 };
