63692e917673b6eecf8ba2ea079477812b8ff268
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-09-01 12:13:59 +0530

    Time: 93 ms (42.61%) | Memory: 54.5 MB (95.15%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
@@ -1,21 +1,47 @@
 class Solution {
-public:
-    long long minimumReplacement(vector<int>& nums) {
+    public: long minimumReplacement(vector<int>& nums) {
         int n = nums.size();
-        long long result = 0;
         
-        for(int i = n-2; i >= 0; i--) {
+        long operations = 0;
 
-            int splits = nums[i]/nums[i+1];
+        for (int i = n - 2; i >= 0; i--) {
             
-            if((nums[i]) % nums[i+1] != 0) {
-                splits++;
+            if (nums[i] <= nums[i + 1]) {
+                continue;
             }
+
+            long parts = (long)(nums[i]) / (long)nums[i + 1];
             
-            nums[i] = nums[i]/splits;
-            result += splits-1;
+            if(nums[i] % nums[i+1] != 0)
+                parts += 1;
+
+            operations += parts - 1;
+
+            nums[i] = nums[i] / (int)parts;
         }
-        
-        return result;
+
+        return operations;
     }
 };
+
+// class Solution {
+// public:
+//     long long minimumReplacement(vector<int>& nums) {
+//         int n = nums.size();
+//         long long result = 0;
+        
+//         for(int i = n-2; i >= 0; i--) {
+
+//             int splits = nums[i]/nums[i+1];
+            
+//             if((nums[i]) % nums[i+1] != 0) {
+//                 splits++;
+//             }
+            
+//             nums[i] = nums[i]/splits;
+//             result += splits-1;
+//         }
+        
+//         return result;
+//     }
+// };
