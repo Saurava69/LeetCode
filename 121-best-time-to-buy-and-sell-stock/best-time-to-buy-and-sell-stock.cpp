abd2bfb59e932cd81204b16f7f82dec958168d8a
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-09-10 18:53:23 +0530

    Time: 260 ms (5.17%) | Memory: 163.1 MB (6.12%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,45 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int buy=INT_MAX,sell,profit=0;
+//         int n=prices.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             if(prices[i]<buy)
+//             {
+//                 buy=prices[i];
+//             }
+//             sell=prices[i]-buy;
+//             if(sell>profit) profit=sell;
+//         }
+//         return profit;
+
+//     }
+// };
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int buy=INT_MAX,sell,profit=0;
-        int n=prices.size();
-        for(int i=0;i<n;i++)
-        {
-            if(prices[i]<buy)
-            {
-                buy=prices[i];
-            }
-            sell=prices[i]-buy;
-            if(sell>profit) profit=sell;
+    int find(vector<int> &prices, int i,int k,bool buy,vector<vector<int>> &v)
+    {
+        //if we have no stock or we have no chance of transaction(k=0)
+        if(i>=prices.size()||k<=0) return 0;
+        if(v[i][buy]!=-1) return v[i][buy];
+        
+        if(buy)  //if we are buying then next time we will sell else next time we will buy
+        {        //-prices[i], because bought stock of prices[i], expend money
+            
+           return v[i][buy]=max(-prices[i]+find(prices,i+1,k,!buy,v),find(prices,i+1,k,buy,v)); 
         }
-        return profit;
+        else    //it's time to sell , now decrease k, we have done 1 transaction
+        {       //+prices[i], because we now gain (i.e) sell our stock at rate of prices[i]
+           return v[i][buy]=max( prices[i]+find(prices,i+1,k-1,!buy,v),find(prices,i+1,k,buy,v)); 
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size(); 
+        vector<vector<int>> v(n,vector<int> (2,-1));
+        //passing here buy=1 because we will first buy then sell
+        //we can do atmost k=1 transaction
+        return find(prices,0,1,1,v);
     }
 };
