fbaf739087394cb897c65fffb82ba4298100ee19
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-09-25 08:56:21 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (16.46%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // char findTheDifference(string s, string t) {
+    //     vector<int> v(26, 0);
+    //     for(auto& c:s) v[c-'a']+=1;
+    //     for(auto& c:t) if(--v[c-'a']<0) return c;
+    //     return '\0';
+    // }
+
+    // Xor method
+    //     char findTheDifference(string s, string t) {
+    //     char ret ='\0';
+    //     for (auto &c: s) ret^=c;
+    //     for (auto &c: t) ret^=c;
+    //     return ret;
+    // }
+
+    //Summation Method
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        for (auto &c: t) (sum+=c)%=256;
+        for (auto &c: s) (sum-=c)%=256;
+        return (char)sum;
+    }
+};
