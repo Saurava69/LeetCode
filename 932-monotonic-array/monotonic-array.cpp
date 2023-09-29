01c231993b33c3fc64dedcaacf5400010e361d16
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-09-29 18:00:53 +0530

    Time: 135 ms (18.04%) | Memory: 96.9 MB (9.17%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,21 +1,45 @@
+// class Solution {
+// public:
+//     bool isMonotonic(vector<int>& nums) {
+//         int t=nums.size()-1;
+//         if(nums[0]>nums[t])
+//         {
+//             for(int i=1;i<=t;i++)
+//             {
+//                 if(nums[i]>nums[i-1]) return false;
+//             }
+//         }
+//         else
+//         {
+//             for(int i=1;i<=t;i++)
+//             {
+//                 if(nums[i]<nums[i-1]) return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        int t=nums.size()-1;
-        if(nums[0]>nums[t])
-        {
-            for(int i=1;i<=t;i++)
-            {
-                if(nums[i]>nums[i-1]) return false;
-            }
+        bool isIncreasing = true;  // Indicates if the array is increasing.
+        bool isDecreasing = true;  // Indicates if the array is decreasing.
+
+        // Check if the array is either increasing or non-increasing.
+        for (int i = 1; i < nums.size(); i++) {
+            // Check increasing condition.
+            if (nums[i] < nums[i - 1])
+                isIncreasing = false;
+                
+            // Check decreasing condition.
+            else if (nums[i] > nums[i - 1])
+                isDecreasing = false;
+
+            // If it is neither increasing nor decreasing then don't continue the loop
+            if (!isIncreasing && !isDecreasing)
+                break;
         }
-        else
-        {
-            for(int i=1;i<=t;i++)
-            {
-                if(nums[i]<nums[i-1]) return false;
-            }
-        }
-        return true;
+
+        return isIncreasing || isDecreasing;  // Return true if either condition is met.
     }
 };
