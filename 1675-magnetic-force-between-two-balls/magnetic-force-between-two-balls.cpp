01c5da5e1bcf0ac6a66e26a7751e30ef73ffcdda
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-10-24 00:48:19 +0530

    Time: 162 ms (55.24%) | Memory: 58.2 MB (43.74%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -5,7 +5,7 @@ public:
         sort(position.begin(),position.end());
         int start,end,n=position.size(),count=1,ans;;
         start=1;
-        end=position[n-1]-position[0];
+        end=position[n-1]-1;
         while(start<=end)
         {
             int mid=start+(end-start)/2;
