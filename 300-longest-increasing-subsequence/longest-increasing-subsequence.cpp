bafcf4f44334431e4301a8753569525bb9d3fe79
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-10-25 20:15:39 +0530

    Time: 527 ms (25.72%) | Memory: 38.3 MB (30.88%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,43 @@
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& arr) {
+//         int n=arr.size();
+//         int lis[n]; 
+
+// 	lis[0] = 1; 
+
+// 	/* Compute optimized LIS values in bottom up manner */
+// 	for (int i = 1; i < n; i++ ) 
+// 	{ 
+// 		lis[i] = 1; 
+// 		for (int j = 0; j < i; j++ ) 
+// 			if ( arr[i] > arr[j] && lis[i] < lis[j] + 1) 
+// 				lis[i] = lis[j] + 1; 
+// 	}
+//     return *max_element(lis,lis+n); 
+//     }
+// };
+class Solution {
+public:
+    int n;
+    int t[2501][2501];
+    int lis(vector<int>& nums, int prev_idx, int curr_idx) {
+       if(curr_idx == n)
+           return 0;
+        
+        if( t[prev_idx+1][curr_idx] != -1)
+            return t[prev_idx+1][curr_idx];
+
+        int not_taken = lis(nums, prev_idx, curr_idx+1);
+        int taken = 0;
+        if(prev_idx == -1 || nums[curr_idx] > nums[prev_idx])
+            taken = 1 + lis(nums, curr_idx, curr_idx+1);
+        return t[prev_idx+1][curr_idx] =  max(taken, not_taken);
+            
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        memset(t, -1, sizeof(t));
+        n = nums.size();
+        return lis(nums, -1, 0);
+    }
+};
