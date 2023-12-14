973c365e12dca763f892205b64f7f30725968c2b
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-12-14 18:59:18 +0530

    Time: 191 ms (72.14%) | Memory: 117.5 MB (76.35%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -28,17 +28,20 @@ public:
     //     }
     //     return row;
     // }
-    int m = grid.size();
-    int n = grid[0].size();
-    vector<int>row(m);
-    vector<int>col(n);
-    vector<vector<int>> ansGrid(m,vector<int>(n));
+    const int m = grid.size();
+    const int n = grid[0].size();
+    vector<int>row(m,0);
+    vector<int>col(n,0);
+    vector<vector<int>> ansGrid(m,vector<int>(n,0));
     for(int i=0;i<m;i++)
     {
         for(int j=0;j<n;j++)
         {
-            row[i]+=grid[i][j];
-            col[j]+=grid[i][j];
+            if(grid[i][j]==1){ 
+                ++row[i];
+                ++col[j];
+            }
+            
         }
     }
     for(int i=0;i<m;i++)
