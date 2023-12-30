840c59a3ae9fe2b7277547b4e69110a6078c79c8
Saurava69 <102845138+Saurava69@users.noreply.github.com>
2023-12-30 10:02:58 +0530

    Time: 201 ms (26.20%) | Memory: 83.7 MB (52.64%) - LeetSync

--- 790-global-and-local-inversions/global-and-local-inversions.cpp
+++ 790-global-and-local-inversions/global-and-local-inversions.cpp
@@ -6,33 +6,26 @@ public:
     bool isIdealPermutation(vector<int>& nums) {
          // First approach 
 
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     if(abs(nums[i] - i) > 1)
-        //    {
-        //        return false;
-        //    } 
-        // }
-        // return true;
-
-        auto fastio=[]()  {
-            std::ios::sync_with_stdio(false);
-            cin.tie(nullptr);
-            cout.tie(nullptr);
-            return nullptr;
-        }();
+        for(int i=0;i<nums.size();i++)
+        {
+            if(abs(nums[i] - i) > 1)
+           {
+               return false;
+           } 
+        }
+        return true;
 
         // faster Second Approach
-        if(nums.size()>2)
-        {
-        int maxi =-1;
+        // if(nums.size()>2)
+        // {
+        // int maxi =-1;
         
-        for(int i=0;i<nums.size()-2;i++)
-        {
-            maxi=max(maxi,nums[i]);
-            if(maxi>nums[i+2]) return false;
-        } 
-        }
-        return true;       
+        // for(int i=0;i<nums.size()-2;i++)
+        // {
+        //     maxi=max(maxi,nums[i]);
+        //     if(maxi>nums[i+2]) return false;
+        // } 
+        // }
+        // return true;       
     }
 };
