272288cf5fb988f739774d9c9243a5dbd0586245
Saurav Kumar <meskmishra1@gmail.com>
2024-01-24 16:16:52 +0530

    Time: 227 ms (97.59%) | Memory: 178.5 MB (96.51%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,38 +1,39 @@
-
-
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int pseudoPalindromicPaths(TreeNode* root) {
-        int count = 0, path = 0;
+    void solve(TreeNode* root, vector<int>&temp,int &result)
+    {
+        if(root==NULL) return;
+        temp[root->val]++;
 
-        stack<pair<TreeNode*, int>> stk;
-        stk.push({root, 0});
-
-        while (!stk.empty()) {
-            auto [node, path] = stk.top();
-            stk.pop();
-
-            if (node != nullptr) {
-                
-                path = path ^ (1 << node->val);
-
-                
-                if (node->left == nullptr && node->right == nullptr) {
-                    
-                    if ((path & (path - 1)) == 0) {
-                        ++count;
-                    }
-                } else {
-                    stk.push({node->right, path});
-                    stk.push({node->left, path});
-                }
+        if(!root->left and !root->right)
+        {
+            int oddCount=0;
+            for(int i=1;i<=9;i++)
+            {
+                if(temp[i]%2!=0) oddCount++;
             }
+            if(oddCount<=1) result+=1;
         }
+        solve(root->left,temp,result);
+        solve(root->right,temp,result);
+        temp[root->val]--;
 
-        return count;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int>temp(10,0);
+        int result=0;
+        solve(root,temp,result);
+        return result;
     }
 };
-
-
-
