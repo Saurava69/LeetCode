def4a3e21c80c7e0cff0c2665f97d1aa50ca21d6
Saurav Kumar <meskmishra1@gmail.com>
2024-09-02 11:16:17 +0530

    Time: 102 ms (30.75%) | Memory: 84 MB (5.65%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long long sum = 0;
-        for(int i=0;i<chalk.size();i++){
-            sum+=chalk[i];
-            if(sum > k) break;
+        int n = chalk.size();
+
+        vector<long> prefixSum(n);
+        prefixSum[0] = chalk[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + chalk[i];
         }
-        k = k % sum;
-        
-        int i =0;
-        while(k>0){
-            k-=chalk[i];
-            if(k < 0) return i;
-            i++;
+
+        long sum = prefixSum[n - 1];
+        long remainingChalk = (k % sum);
+
+        return binarySearch(prefixSum, remainingChalk);
+    }
+
+private:
+    int binarySearch(vector<long>& arr, long tar) {
+        int low = 0, high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] < tar) {
+                low = mid + 1;
+            }
+            else if(arr[mid] == tar){
+                return mid+1;
+            } else {
+                high = mid-1;
+            }
         }
-        return i;
+
+        return high+1;
     }
 };
