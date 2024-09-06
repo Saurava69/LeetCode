991221b5190330cb87d96637ad40083cf3694e05
Saurav Kumar <meskmishra1@gmail.com>
2024-09-06 11:27:03 +0530

    Time: 402 ms (93.14%) | Memory: 258.5 MB (86.15%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -8,32 +8,41 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int>st;
-        for(auto &x:nums){
+        // Create an unordered set to store the elements from the vector 'nums'
+        unordered_set<int> st;
+        for(auto &x : nums) {
             st.insert(x);
         }
-        while((st.contains(head->val))){
+        
+        // Remove nodes from the beginning of the list if they are present in the set
+        while (head && st.contains(head->val)) {
             head = head->next;
-            
         }
-        ListNode* prev = head;
-        ListNode* temp = head;
-        while(temp){
-            if(st.contains(temp->val)){
-                ListNode* td = temp;
-                while( temp and st.contains(temp->val)  ){
-                        temp = temp->next;
+        
+        // If the head becomes null (i.e., all nodes were removed), return null
+        if (!head) return nullptr;
+        
+        // Initialize pointers to traverse the list
+        ListNode* prev = head;  // 'prev' will keep track of the last valid node
+        ListNode* temp = head;  // 'temp' is used to traverse the list
+        
+        while (temp) {
+            // If the current node's value is in the set, skip all consecutive nodes that are also in the set
+            if (st.contains(temp->val)) {
+                while (temp && st.contains(temp->val)) {
+                    temp = temp->next;  // Move 'temp' to the next node
                 }
-                prev->next = temp;
-                prev =temp;
+                prev->next = temp;  // Link 'prev' to the first node after the sequence of nodes to be removed
             }
-            prev =temp;
-            if(temp)
-            temp = temp->next;
+            // Move 'prev' to 'temp' only if 'temp' is not null
+            prev = temp;
+            if (temp) temp = temp->next;  // Move 'temp' to the next node in the list
         }
-        return head;
+        
+        return head;  // Return the modified head of the list
     }
 };
