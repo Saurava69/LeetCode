b47118a9092609e6278fff66c2331d925ad04df4
Saurav Kumar <meskmishra1@gmail.com>
2025-02-22 12:45:52 +0530

    Time: 3 ms (45.17%) | Memory: 12.9 MB (30.46%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
-        stack<int> s;
-        unordered_map<int, int> m;
-        for (int n : nums) {
-            while (s.size() && s.top() < n) {
-                m[s.top()] = n;
-                s.pop();
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> ngeMap;  // Map to store the next greater element for each number in nums2
+        stack<int> st;  // Monotonic decreasing stack to track the next greater element
+
+        // Iterate over nums2 from right to left (reverse order)
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            // Pop elements from the stack that are smaller than or equal to nums2[i]
+            while (!st.empty() && st.top() < nums2[i]) {
+                st.pop();  // Remove smaller elements, they cannot be the NGE
             }
-            s.push(n);
+
+            // If stack is empty, there is no greater element to the right
+            if (st.empty()) {
+                ngeMap[nums2[i]] = -1;  // No NGE found, assign -1
+            } else {
+                ngeMap[nums2[i]] = st.top();  // Assign the top of the stack as NGE
+            }
+
+            // Push the current element onto the stack for future comparisons
+            st.push(nums2[i]);
         }
-        vector<int> ans;
-        for (int n : findNums) ans.push_back(m.count(n) ? m[n] : -1);
-        return ans;
+
+        // Prepare the result for nums1 by looking up the values in ngeMap
+        vector<int> result(nums1.size());
+        for (int i = 0; i < nums1.size(); i++) {
+            result[i] = ngeMap[nums1[i]];  // Retrieve precomputed NGE from the map
+        }
+
+        return result;  // Return the final result
     }
 };
