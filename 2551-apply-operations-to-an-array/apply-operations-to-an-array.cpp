432741cca8df67f23f0780172a8f5ffc52e7989d
Saurav Kumar <meskmishra1@gmail.com>
2025-03-01 06:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (87.59%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(n,0);
+        vector<int>ans(n);
         int l=0;
         for(int i=0;i<n-1;i++){
             if(nums[i]==nums[i+1]){
