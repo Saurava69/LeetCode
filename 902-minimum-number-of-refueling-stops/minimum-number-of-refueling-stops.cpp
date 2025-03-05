d75c844e4eb097974a57c109eaa08f77f6b14c20
Saurav Kumar <meskmishra1@gmail.com>
2025-03-06 02:36:10 +0530

    Time: 59 ms (8.34%) | Memory: 113 MB (5.93%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    // Method to find the minimum number of refueling stops required
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        priority_queue<int>pq;
-
-        int max_reach = startFuel;
-        int count = 0;
-        int n= stations.size();
-        int i= 0;
-        while(max_reach<target){
-            while(i<n and stations[i][0]<=max_reach){
-                pq.push( stations[i][1]);
-                i++;
+        int n = stations.size();
+        vector<vector<long>> dp(n + 1, vector<long>(n + 1, 0)); // dp[i][j] -> max distance with j stops up to i-th station
+        
+        // Base case: With 0 stops, we can reach only as far as startFuel allows
+        dp[0][0] = startFuel;
+        
+        for (int i = 1; i <= n; i++) {
+            int stationPos = stations[i - 1][0]; // Position of current station
+            int stationFuel = stations[i - 1][1]; // Fuel available at this station
+            
+            for (int j = 0; j <= i; j++) {  
+                // If we don't take fuel from this station
+                dp[i][j] = dp[i - 1][j];
+                
+                // If we take fuel (only if we made at least one stop)
+                if (j > 0 && dp[i - 1][j - 1] >= stationPos) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + stationFuel);
+                }
             }
-            if(pq.empty()) return -1;
-            max_reach+=pq.top();
-            pq.pop();
-            count++;
         }
-        return count;
+        
+        // Find the minimum stops required to reach target
+        for (int j = 0; j <= n; j++) {
+            if (dp[n][j] >= target) return j;
+        }
+        
+        return -1; // Target is unreachable
     }
 };
