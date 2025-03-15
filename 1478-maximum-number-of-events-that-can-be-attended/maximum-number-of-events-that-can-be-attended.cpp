6e648bbf9188ad523274b894a8c52b68e32d1df7
Saurav Kumar <meskmishra1@gmail.com>
2025-03-16 01:18:08 +0530

    Time: 62 ms (67.10%) | Memory: 75 MB (47.16%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-static bool cmp(vector<int>&a,vector<int>&b){
-    if(a[1]==b[1]){
-        return a[0]<=b[0];
-    }
-    return a[1]<b[1];
-}
     int maxEvents(vector<vector<int>>& A) {
+        //create min Heap
         priority_queue <int, vector<int>, greater<int>> pq;
         sort(A.begin(), A.end());
-        int i = 0, res = 0, n = A.size();
-        for (int d = 1; d <= 100000; ++d) {
-            while (pq.size() && pq.top() < d)
-                pq.pop();
-            while (i < n && A[i][0] == d)
+        int i = 0, res = 0, n = A.size(),d=0;
+        while(i<n or !pq.empty()){
+            if(pq.empty()) d = A[i][0];
+            //insert into priority queue if event start day is less than or eequal to curray day
+            while(i<n and A[i][0]<=d){
                 pq.push(A[i++][1]);
-            if (pq.size()) {
+            }
+            pq.pop();
+            d++;
+            res++;
+            //clear the events which canot able to make within d days
+            while(!pq.empty() and pq.top()<d){
                 pq.pop();
-                ++res;
             }
         }
         return res;
