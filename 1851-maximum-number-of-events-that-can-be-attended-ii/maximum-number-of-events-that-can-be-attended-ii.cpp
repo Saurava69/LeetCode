107db74c21b640b52725e567a0468ab618d1feb8
Saurav Kumar <meskmishra1@gmail.com>
2025-03-17 18:58:13 +0530

    Time: 207 ms (15.71%) | Memory: 112.7 MB (92.64%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,31 +1,50 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-    int solve(vector<vector<int>>&dp, vector<vector<int>>& events, int n, int pos, int k)
-{
-        int i;
-        
-        if(pos >= n || k == 0)
-            return 0;
-        
-        if(dp[pos][k] != -1)
-            return dp[pos][k];
-        
-        // finding the next event which we can attend
-        for(i = pos + 1; i < n; i++)
-            if(events[i][0] > events[pos][1])
-                break;
-        
-       return dp[pos][k] = max(solve(dp, events, n, pos+1, k), events[pos][2] + solve(dp, events, n, i, k-1));
-
-    }
-    
-    int maxValue(vector<vector<int>>& events, int k) 
-    {
+    int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
-        sort(events.begin(), events.end());
-        
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        
-        return solve(dp, events, n, 0, k);
+        sort(events.begin(), events.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] < b[1];  // Sort by end time
+        });
+
+        // DP table
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+
+        // Binary search to find last non-overlapping event
+        auto findLastNonOverlap = [&](int i) {
+            int left = 0, right = i - 1, ans = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (events[mid][1] < events[i][0]) {  // Find event ending before i-th event starts
+                    ans = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return ans;
+        };
+
+        // DP iteration
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= k; ++j) {
+                // Option 1: Skip current event
+                dp[i][j] = dp[i - 1][j];
+
+                // Option 2: Take the event
+                int lastNonOverlap = findLastNonOverlap(i - 1);
+                int take = events[i - 1][2];  // Value of current event
+                if (lastNonOverlap != -1) {
+                    take += dp[lastNonOverlap + 1][j - 1];
+                }
+
+                dp[i][j] = max(dp[i][j], take);
+            }
+        }
+
+        return dp[n][k];
     }
 };
