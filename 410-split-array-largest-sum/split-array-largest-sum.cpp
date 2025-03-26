ed33a9bca224daa5780bf26938f93c2df3640bbc
Saurav Kumar <meskmishra1@gmail.com>
2025-03-27 00:49:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.30%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,57 +1,33 @@
-//Optimized DP with Monotonic Property (Using Convex Hull Trick / Divide & Conquer DP)
-
 class Solution {
 public:
-    int n;
-    vector<int> prefixSum;
-    vector<vector<int>> dp;
-    
-    // Cost function: Sum from p+1 to i
-    int cost(int p, int i) {
-        return prefixSum[i] - prefixSum[p];
-    }
-
-    // Recursive Divide & Conquer DP
-    void compute(int j, int left, int right, int optLeft, int optRight) {
-        if (left > right) return;
-        
-        int mid = (left + right) / 2;
-        int bestP = -1;
-        dp[j][mid] = INT_MAX;
-
-        for (int p = optLeft; p <= min(mid, optRight); p++) {
-            int currCost = max(dp[j-1][p], cost(p, mid));
-            if (currCost < dp[j][mid]) {
-                dp[j][mid] = currCost;
-                bestP = p;
+    bool isValidSplit(vector<int>& nums, int k, int maxSum) {
+        int subarrays = 1, currentSum = 0;
+        for (int num : nums) {
+            if (currentSum + num > maxSum) {
+                subarrays++;
+                currentSum = num;
+                if (subarrays > k) return false;
+            } else {
+                currentSum += num;
             }
         }
-
-        // Divide & Conquer: Process left and right halves
-        compute(j, left, mid - 1, optLeft, bestP);
-        compute(j, mid + 1, right, bestP, optRight);
+        return true;
     }
 
     int splitArray(vector<int>& nums, int k) {
-        n = nums.size();
-        prefixSum.assign(n + 1, 0);
-        dp.assign(k + 1, vector<int>(n + 1, INT_MAX));
+        int left = *max_element(nums.begin(), nums.end());
+        int right = accumulate(nums.begin(), nums.end(), 0);
+        int result = right;
 
-        // Compute prefix sums
-        for (int i = 1; i <= n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isValidSplit(nums, k, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-
-        // Base case: One partition (prefix sum)
-        for (int i = 1; i <= n; i++) {
-            dp[1][i] = prefixSum[i];
-        }
-
-        // Solve DP using Divide & Conquer optimization
-        for (int j = 2; j <= k; j++) {
-            compute(j, j, n, j - 1, n - 1);
-        }
-
-        return dp[k][n];
+        return result;
     }
 };
