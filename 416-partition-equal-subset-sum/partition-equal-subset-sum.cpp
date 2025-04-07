8ffbf108ee5733ea5c0f26befa25b9cf2e556c73
Saurav Kumar <meskmishra1@gmail.com>
2025-04-07 20:59:48 +0530

    Time: 108 ms (65.29%) | Memory: 17.1 MB (62.07%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int dp[202][20001];
-    bool rec(int i, vector<int>& nums, int s, int sum) {
-        if (s == sum) return true;
-        if (i >= nums.size() || s > sum) return false;
-        if(dp[i][s]!=-1) return dp[i][s];
-
-        bool take = rec(i + 1, nums, s + nums[i], sum);
-        bool notTake = rec(i + 1, nums, s, sum);
-        return dp[i][s]=take || notTake;
-    }
-
     bool canPartition(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if (sum % 2 != 0) return false;
-        return rec(0, nums, 0, sum / 2);
+
+        int target = sum / 2;
+        int n = nums.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+
+        // Base case: sum 0 is always achievable
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = true;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= target; ++j) {
+                if (j < nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j]; // Can't include current num
+                } else {
+                    // Either take or don't take the current num
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[n][target];
     }
 };
