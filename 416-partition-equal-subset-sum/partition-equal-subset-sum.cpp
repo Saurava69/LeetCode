8aa0c4fdfd2efc3a54841935b6c253040d5cf664
Saurav Kumar <meskmishra1@gmail.com>
2025-04-07 20:52:49 +0530

    Time: 203 ms (11.53%) | Memory: 30.7 MB (52.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
+    int dp[202][20001];
+    bool rec(int i, vector<int>& nums, int s, int sum) {
+        if (s == sum) return true;
+        if (i >= nums.size() || s > sum) return false;
+        if(dp[i][s]!=-1) return dp[i][s];
+
+        bool take = rec(i + 1, nums, s + nums[i], sum);
+        bool notTake = rec(i + 1, nums, s, sum);
+        return dp[i][s]=take || notTake;
+    }
+
     bool canPartition(vector<int>& nums) {
-        int totalSum = 0;
-        for (int num : nums) totalSum += num;
-
-        if (totalSum % 2 != 0) return false;
-
-        int targetSum = totalSum / 2;
-        vector<bool> dp(targetSum + 1, false);
-        dp[0] = true;
-        for (int num : nums) {
-            for (int currSum = targetSum; currSum >= num; --currSum) {
-                dp[currSum] = dp[currSum] || dp[currSum - num];
-                if (dp[targetSum]) return true;
-            }
-        }
-        return dp[targetSum];
+        memset(dp,-1,sizeof(dp));
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 != 0) return false;
+        return rec(0, nums, 0, sum / 2);
     }
 };
