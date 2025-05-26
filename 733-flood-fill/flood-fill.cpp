c8d88d64b859b6879684d71097d0baf3060e4196
Saurav Kumar <meskmishra1@gmail.com>
2025-05-26 21:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (35.41%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,34 +1,25 @@
 class Solution {
-private:
-    void dfs(int row, int col, vector<vector<int>>&ans,
-     vector<vector<int>>& image, int newColor, int delRow[], int delCol[],
-     int iniColor) {
-        // color with new color
-        ans[row][col] = newColor; 
-        int n = image.size();
-        int m = image[0].size(); 
-        // there are exactly 4 neighbours
-        for(int i = 0;i<4;i++) {
-            int nrow = row + delRow[i]; 
-            int ncol = col + delCol[i]; 
-            // check for valid coordinate 
-            // then check for same initial color and unvisited pixel
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
-            image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor); 
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size(), m = image[0].size();
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        int c = image[sr][sc];
+       // vector<vector<int>>visited=image;
+        if( c==color) return image;
+        image[sr][sc] = color;
+        while(!q.empty()){
+            auto [x,y] = q.front();
+            q.pop();
+            vector<pair<int,int>>dir = {{0,1},{0,-1},{-1,0},{1,0}};
+            for(auto &[dx,dy]:dir){
+                int u = x+dx, v = y + dy;
+                if(u<n and u>=0 and v<m and v>=0 and image[u][v]==c){
+                    image[u][v] = color;
+                    q.push({u,v});
+                }
             }
         }
-    }
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, 
-    int sr, int sc, int newColor) {
-        // get initial color
-        int iniColor = image[sr][sc]; 
-        vector<vector<int>> ans = image; 
-        // delta row and delta column for neighbours
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1}; 
-        dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor); 
-        return ans; 
+        return image;
     }
 };
