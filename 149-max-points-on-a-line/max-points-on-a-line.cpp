5076e0f1a8ec792611cf173a16cc74250086ee40
Saurav Kumar <meskmishra1@gmail.com>
2026-05-13 02:59:19 +0530

    Time: 23 ms (63.54%) | Memory: 15.6 MB (74.66%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    double slope(vector<int>&point1, vector<int>&point2){
-        int x1 = point1[0], x2 = point2[0], y1 = point1[1], y2 = point2[1];
-        return (x2-x1) != 0 ? double(y2-y1)/(x2-x1) : INT_MAX;
-    }
     int maxPoints(vector<vector<int>>& points) {
-        int ans = 0, n = points.size();
+        int n = points.size();
+        if(n <= 2) return n;
 
-        for(int i=0;i<n;i++){
-            unordered_map<double,int>mp;
-            for(int j=0;j<n;j++){
-                if(i!=j){
-                    double sl = slope(points[i],points[j]);
-                    mp[sl]++;
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            map<pair<int,int>, int> mp;
+
+            int currMax = 0;
+
+            for(int j = i + 1; j < n; j++) {
+
+                int dy = points[j][1] - points[i][1];
+                int dx = points[j][0] - points[i][0];
+
+                if(dx == 0) {
+                    dy = 1;
                 }
+                else if(dy == 0) {
+                    dx = 1;
+                }
+                else {
+
+                    int g = gcd(abs(dx), abs(dy));
+
+                    dx /= g;
+                    dy /= g;
+
+                    if(dx < 0) {
+                        dx *= -1;
+                        dy *= -1;
+                    }
+                }
+
+                mp[{dy, dx}]++;
+
+                currMax = max(currMax, mp[{dy, dx}]);
             }
-            for(auto [x,y]:mp){
-                ans = max(ans,y);
-            }
+
+            ans = max(ans, currMax + 1);
         }
-        return ans+1;
+
+        return ans;
     }
 };
