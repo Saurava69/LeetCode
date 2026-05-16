16cfacba18b26146ef8753b3244e4fdbb4aeba99
Saurav Kumar <meskmishra1@gmail.com>
2026-05-17 02:27:07 +0530

    Time: 12 ms (55.32%) | Memory: 21.7 MB (44.16%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,37 +1,31 @@
+//clean code
 class Solution {
 public:
-    
-    int solve(int i,int j,vector<vector<int>>& mat,vector<vector<int>> &dp){
-        int m = mat.size(),n = mat[0].size();
-        if(i<0 or j<0 or i>=m or j>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = 1;
-        if(i<m-1 and mat[i+1][j]>mat[i][j]){
-            down = 1+solve(i+1,j,mat,dp);
-        }
-        int up = 1;
-        if(i>0 and mat[i-1][j]>mat[i][j]){
-            up = 1+solve(i-1,j,mat,dp);
-        }
-        int right = 1;
-        if(j<n-1 and mat[i][j+1]>mat[i][j]){
-            right = 1+solve(i,j+1,mat,dp);
-        }
-        int left = 1;
-        if(j>0 and mat[i][j-1]>mat[i][j]){
-            left = 1+solve(i,j-1,mat,dp);
-        }
-        return dp[i][j]=max({left,right,up,down});
-    }
-    int longestIncreasingPath(vector<vector<int>>& mat) {
-        int ans = 1;
-        int m = mat.size(),n = mat[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans = max(ans,solve(i,j,mat,dp));
+    int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    vector<vector<int>> dp;
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        dp.assign(m, vector<int>(n, 0));
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result = max(result, dfs(matrix, i, j, m, n));
             }
         }
-        return ans;
+        return result;
+    }
+
+    int dfs(vector<vector<int>>& matrix, int i, int j, int m, int n) {
+        if (dp[i][j] != 0) return dp[i][j];
+        int best = 1;
+        for (auto& d : dirs) {
+            int ni = i + d[0], nj = j + d[1];
+            if (ni >= 0 && ni < m && nj >= 0 && nj < n && matrix[ni][nj] > matrix[i][j]) {
+                best = max(best, 1 + dfs(matrix, ni, nj, m, n));
+            }
+        }
+        dp[i][j] = best;
+        return best;
     }
 };
