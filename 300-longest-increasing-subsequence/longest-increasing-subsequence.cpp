9457fb6f36c817d28cbf74b34df8b86c5a3da647
Saurav Kumar <meskmishra1@gmail.com>
2026-05-17 18:54:44 +0530

    Time: 70 ms (48.72%) | Memory: 14.3 MB (56.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& arr) {
-        int n=arr.size();
-        int lis[n]; 
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int maxLen = 1;
 
-	lis[0] = 1; 
-    int maxi=lis[0];
-	/* Compute optimized LIS values in bottom up manner */
-	for (int i = 1; i < n; i++ ) 
-	{ 
-		lis[i] = 1; 
-		for (int j = i-1; j >=0; j-- ) 
-			if ( arr[i] > arr[j] && lis[i] < lis[j] + 1) 
-				lis[i] = lis[j] + 1; 
-    maxi=max(maxi,lis[i]);
-	}
-    return maxi; 
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+
+        return maxLen;
     }
 };
-// class Solution {
-// public:
-//     int n;
-//     int t[2501][2501];
-//     int lis(vector<int>& nums, int prev_idx, int curr_idx) {
-//        if(curr_idx == n)
-//            return 0;
-        
-//         if( t[prev_idx+1][curr_idx] != -1)
-//             return t[prev_idx+1][curr_idx];
-
-//         int not_taken = lis(nums, prev_idx, curr_idx+1);
-//         int taken = 0;
-//         if(prev_idx == -1 || nums[curr_idx] > nums[prev_idx])
-//             taken = 1 + lis(nums, curr_idx, curr_idx+1);
-//         return t[prev_idx+1][curr_idx] =  max(taken, not_taken);
-            
-//     }
-//     int lengthOfLIS(vector<int>& nums) {
-//         memset(t, -1, sizeof(t));
-//         n = nums.size();
-//         return lis(nums, -1, 0);
-//     }
-// };
