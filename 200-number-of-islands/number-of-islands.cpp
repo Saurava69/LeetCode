34440786f8cf2394458341f652c7d6c2d81e8226
Saurav Kumar <meskmishra1@gmail.com>
2026-05-19 09:42:11 +0530

    Time: 29 ms (41.78%) | Memory: 16.2 MB (88.51%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+private:
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        // Out of bounds or water -- stop
+        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col] == '0') {
+            return;
+        }
+        // Sink the current cell to mark it visited
+        grid[row][col] = '0';
+        // Explore all four directions
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+};
